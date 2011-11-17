381ce5171da54a53a32ab6c349525132cafd5f53
feinerer <feinerer@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-11-17 15:45:31 +0000

    Add SMART stopword list
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/tm@1153 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/stopwords.R
+++ pkg/R/stopwords.R
@@ -1,6 +1,6 @@
 # Map IETF language tags to languages used by the Snowball stemmer
 # http://en.wikipedia.org/wiki/IETF_language_tag
-map_IETF <- function(code) {
+map_IETF_Snowball <- function(code) {
     codes <- c("da", "nl", "en", "fi", "fr", "de", "hu", "it", "no", "pt", "ru", "es", "sv")
     names <- c("danish", "dutch", "english", "finnish", "french", "german", "hungarian",
                "italian", "norwegian", "portuguese", "russian", "spanish", "swedish")
@@ -9,15 +9,15 @@ map_IETF <- function(code) {
 }
 
 stopwords <- {
-    function(language = "en") {
-        resolved <- tm:::map_IETF(language)
-        lang <- if (is.na(resolved))
-            language
+    function(kind = "en") {
+        resolved <- tm:::map_IETF_Snowball(kind)
+        base <- if (is.na(resolved))
+            kind
         else
             resolved
-        s <- system.file("stopwords", paste(lang, ".dat", sep = ""), package = "tm")
+        s <- system.file("stopwords", paste(base, ".dat", sep = ""), package = "tm")
         if (identical(s, ""))
-            stop(paste("no stopwords available for '", lang, "'", sep = ""))
+            stop(paste("no stopwords available for '", base, "'", sep = ""))
         readLines(s, encoding = "UTF-8")
     }
 }
