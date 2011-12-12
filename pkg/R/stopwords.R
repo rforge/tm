be264bc11bee91ebd58f919838e845a255a29d43
feinerer <feinerer@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-12-12 06:42:28 +0000

    Map empty input to 'porter'
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/tm@1164 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/stopwords.R
+++ pkg/R/stopwords.R
@@ -1,18 +1,10 @@
-# Map IETF language tags to languages used by the Snowball stemmer
-# http://en.wikipedia.org/wiki/IETF_language_tag
-map_IETF_Snowball <- function(code) {
-    codes <- c("da", "nl", "en", "fi", "fr", "de", "hu", "it", "no", "pt", "ru", "es", "sv")
-    names <- c("danish", "dutch", "english", "finnish", "french", "german", "hungarian",
-               "italian", "norwegian", "portuguese", "russian", "spanish", "swedish")
-
-    names[charmatch(gsub("-.*", "", code), codes)]
-}
-
 stopwords <- {
     function(kind = "en") {
         resolved <- tm:::map_IETF_Snowball(kind)
         base <- if (is.na(resolved))
             kind
+        else if (identical(resolved, "porter"))
+            "english"
         else
             resolved
         s <- system.file("stopwords", paste(base, ".dat", sep = ""), package = "tm")
