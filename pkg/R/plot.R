0406366e37876d7ab3ebc77b6688270fd33c276d
khornik <khornik@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2015-05-05 05:50:38 +0000

    Tweak.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/tm@1424 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/plot.R
+++ pkg/R/plot.R
@@ -21,7 +21,7 @@ function(x,
         i <- 1
         lw <- round(c[lower.tri(c) & c >= corThreshold] * 10)
         for (ae in Rgraphviz::AgEdge(p)) {
-            lines(ae, lwd = lw[i], len = 1)
+            Rgraphviz::lines(ae, lwd = lw[i], len = 1)
             i <- i + 1
         }
     }
